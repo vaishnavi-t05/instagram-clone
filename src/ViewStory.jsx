57a2fc4aaed74de5e4afe3f36b006b7750a78f56
vaishnavi-t05 <tvaishnavi2205@gmail.com>
2026-07-13 20:13:33 +0530

    useparams

--- src/ViewStory.jsx
+++ src/ViewStory.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const ViewStory = () => {
+
+  const {id} = useParams();
+
+  const [story, setStory] = useState(null);
+
+  useEffect(()=>{
+    fetch(`http://localhost:3000/story/${id}`)
+    .then(data => data.json())
+    .then(data => setStory(data))
+    .catch(err => console.log(err))
+
+  },[]);
+
   return (
     <div>
+      {story? <div>
+        <img src={story.image} alt="" />
+      </div> : 
       
+      <div>Loading</div>}
     </div>
   )
 }
