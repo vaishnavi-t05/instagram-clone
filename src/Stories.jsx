146147a8abe7d05da3bec63649a2a94e4ccec429
vaishnavi-t05 <tvaishnavi2205@gmail.com>
2026-09-23 13:19:17 +0530

    fix stories: restore usernames and your-story tile

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -22,20 +22,20 @@ console.log(stories);
 
         </div>
       {/* Your story tile (mobile app style) */}
-      {/* <div className='mx-1 d-flex flex-column align-items-center' onClick={()=>{if(stories.length>0) navigate(`/story/1/${stories.length}`)}}>
+      <div className='mx-1 d-flex flex-column align-items-center' onClick={()=>{if(stories.length>0) navigate(`/story/1/${stories.length}`)}}>
         <div className='gradient-border story-mine'>
           <img src={myProfile.profilePic} alt="dp" className='story-dp'/>
           <span className='story-add'>+</span>
         </div>
         <p className='story-user'>Your story</p>
-      </div> */}
+      </div>
       {stories. length > 0 ? (
           stories.map((story)=>(
             <div key={story.id} className='mx-1 d-flex flex-column align-items-center' onClick={()=>{navigate(`/story/${story.id}/${tot}`)}}>
               <div className='gradient-border'>
                 <img src={story.profilePic} alt="dp"  className='story-dp'/>
               </div>
-              <p className='story-user'>Your story</p>
+              <p className='story-user'>{story.username}</p>
             </div>
 
         ))
